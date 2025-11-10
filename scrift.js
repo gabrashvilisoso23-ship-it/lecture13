0207cef234e44fece14f2792d1bf6ccf76ddcfdd
gabrashvilisoso23-ship-it <gabrashvilisoso23@gmail.com>
2025-11-10 12:17:43 +0400

    cretive. html

---
+++ scrift.js
@@ -0,0 +1,12 @@
+let username = "soso";
+console.warn(username)
+let lastname = "gabrashvili";
+console.warn(lastname)
+let age = "18";
+console.warn(age)
+let email = "gabrashvilisoso@gmail.com"
+console.warn(email)
+let phone = 599883791
+console.error(phone)
+Boolean ="ture & false"
+undefined
